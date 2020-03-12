0f5e67bb30c9c351207a6ff3329602d05c4b1bf4
Nguyen Xuan Hoang <hoang.nguyen.connectiv@gmail.com>
2020-03-12 17:19:38 +0700

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Coreada',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'August 27 , 2018'
+    },
+    {
+        'author': 'Coreada',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'August 27 , 2018'
+    },
+    {
+        'author': 'Coreada',
+        'title' : 'Blog Post 3',
+        'content' : 'First post content',
+        'date_posted' : 'August 27 , 2018'
+    },
+    {
+        'author': 'Coreada',
+        'title' : 'Blog Post 4',
+        'content' : 'First post content',
+        'date_posted' : 'August 27 , 2018'
+    },
+]
+
+def home(request) :
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html',context )
+
+def about(request) :
+    return render(request,'blog/about.html',{'title': 'About'} )
